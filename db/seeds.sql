3f64120ce6c8d81be7dae862f9f0c95d733d4deb
Tom Pocius <poci14@gmail.com>
2019-12-15 22:48:28 -0600

    created handlebars folder/files that mirror previous homework assignment for burgers

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Black bean burger');
+INSERT INTO burgers (name) VALUES ('Happy Meal');
+INSERT INTO burgers (name, eaten) VALUES ('Good Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('in and out burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('cheese burger', true);
+INSERT INTO burgers (name) VALUES ('vegan burger');
---
+++ public/assets/js/burger.js
@@ -0,0 +1,53 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-eaten").on("click", function(event) {
+    var id = $(this).data("id");
+    var newEaten = $(this).data("iseaten");
+    console.log("thisdataeaten", $(this).data("iseaten"));
+
+    var newEatenState = {
+      eaten: newEaten
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newEatenState
+    }).then(function() {
+      console.log("changed eaten to", newEaten);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#ca")
+        .val()
+        .trim(),
+      eaten: $("[name=eaten]:checked")
+        .val()
+        .trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function() {
+      console.log("created new cat");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".js-delete").on("click", function(event) {
+    const id = $(this).data("id");
+    $.ajax("/api/burgers/" + id, { type: "DELETE" }).then(() => {
+      location.reload();
+    });
+  });
+});
